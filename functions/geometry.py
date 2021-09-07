564a1025d65c43a9cc9f24ef49c65180cc2bb8cc
cooperhankun <cooperhanson@gmail.com>
2021-09-07 10:46:04 +0200

    ok

---
+++ functions/geometry.py
@@ -0,0 +1,11 @@
+def trianle_area(base, height):
+    return base*height/2
+
+def traingle_circuference(side1, side2, side3):
+    return sum(side1, side2, side3)
+
+def square_area(side):
+    return side**2
+
+def square_circuference(side):
+    return side*4
---
+++ functions/main.py
@@ -0,0 +1,5 @@
+import geometry as geo
+
+traingle_area = geo.traingle_area(3,5)
+print(traingle_area)
+
